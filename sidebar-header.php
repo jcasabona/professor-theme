c3d43b30cbbddfd1f369230d65d85fea803349c8
Joe Casabona <jcasabona@gmail.com>
2014-09-08 11:56:49 -0400

    Assignment and Notes Support
    Added support for assignments & lecture notes

--- sidebar-header.php
+++ sidebar-header.php
@@ -1,8 +1,7 @@
-<section class="card">
+<section class="card group">
 	<?php 
 		if(is_singular('prof_courses')){
 	?>
-		<h2><?php the_title(); ?></h2>
 		<?php $course_meta= prof_get_course_meta($post->ID); ?>
 		<div class="right assns">
 			<?php
@@ -11,17 +10,23 @@
 					echo '<h4>Upcoming Assignments</h4>';
 					echo '<ol>';
 					while($assns->have_posts()) :
+
 						$assns->the_post();
 						$assn_info = get_post_custom(get_the_id()); 
 						$due_date= $assn_info['duedate'][0];
+
+						if($due_date >= date('Y-m-d')) :
 				?>
-						<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <br/>Due: <?php echo $due_date; ?></li>
+							<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <br/>Due: <?php echo $due_date; ?></li>
 				<?php
+						endif;
 					endwhile;
 					echo '</ol>';
 				endif;
+				wp_reset_query();
 			?>
 		</div>
+		<h2><?php the_title(); ?></h2>
 		<ul class="course-meta">
 			<li>Course ID: <?php print $course_meta['courseid']; ?></li>
 			<li>Room: <?php print $course_meta['classroom']; ?></li>
--- single-prof_courses.php
+++ single-prof_courses.php
@@ -9,12 +9,52 @@ get_header();
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-		<article <?php post_class('card') ?> id="post-<?php the_ID(); ?>">
+		<article <?php post_class('card group') ?> id="post-<?php the_ID(); ?>">
 
 			<div class="entry">
 				<?php the_content('<p class="serif">Read the rest of this entry &raquo;</p>'); ?>				
 
 			</div>
+		</article>
+
+		<article class="notes card" id="profNotes">
+			<h3>Lecture Notes</h3>
+			<?php
+				$notes= prof_get_course_notes($post->ID);
+				if($notes->have_posts()) : 
+					echo '<ul>';
+					while($notes->have_posts()) :
+
+						$notes->the_post();
+					?>
+						<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+				<?php
+					endwhile;
+					echo '</ul>';
+				endif;
+				wp_reset_query();
+			?>
+		</article>
+
+		<article class="assignments card" id="profAssignments">
+			<h3>Assignments</h3>
+			<?php
+				$assns= prof_get_course_assignments($post->ID);
+				if($assns->have_posts()) : 
+					echo '<dl>';
+					while($assns->have_posts()) :
+
+						$assns->the_post();
+						$assn_info = get_post_custom(get_the_id()); 
+						$due_date= $assn_info['duedate'][0];
+					?>
+						<dt><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> - Due: <?php print $due_date; ?></dt>
+						<dd><?php the_excerpt(); ?></dd>
+				<?php
+					endwhile;
+					echo '</dl>';
+				endif;
+			?>
 		</article>
 	<?php endwhile; else: ?>
 
